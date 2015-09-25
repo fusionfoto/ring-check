f04b693e446e488a1cbda34afc89f213f1b43df1
Clay Gerrard <clay@swiftstack.com>
2015-09-25 07:00:08 +0000

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+
+setup(
+    name='ring-check',
+    version='0.1',
+    description='Swift Ring Checker',
+    author='SwiftStack',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [],
+    },
+)
